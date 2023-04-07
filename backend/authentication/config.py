a7cac2a797dffdf1bdc10330ec6446f8ff592caf
matkrasinski <mat.krasinski@o2.pl>
2023-04-07 16:34:28 +0200

    Login endpoint, JWT, request filter

---
+++ backend/authentication/config.py
@@ -0,0 +1,8 @@
+class Config:
+    DEBUG: bool = True
+    TESTING: bool = False
+    SECRET_KEY: str = ""
+    ALGORITHM: str = ""
+    EXP_TIME_MIN: float = 0
+    DATABASE_URI: str = ""
+    # Need to be provided
