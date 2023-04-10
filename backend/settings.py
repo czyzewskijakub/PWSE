429e89fc92719503b831f6d7298e5460f415dae9
czyzewja <jakub.czyzewski@tomtom.com>
2023-04-10 23:03:26 +0200

    Include sqlite db and add URI to config.

--- backend/settings.py
+++ backend/settings.py
@@ -6,6 +6,6 @@ TESTING: bool = False
 SECRET_KEY: str = ""
 ALGORITHM: str = ""
 EXP_TIME_MIN: float = 0
-SQLALCHEMY_DATABASE_URI: str = ""
+SQLALCHEMY_DATABASE_URI: str = "sqlite:////pwse/sqlite.db"
 
 
