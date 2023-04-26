c872dc1a8b4592c0b58b6d30d18d4eaa97328460
Michal Andrzejczak <236489>
2023-04-26 21:02:03 +0200

    Views refactor.

--- backend/ai/views.py
+++ backend/ai/views.py
@@ -1,10 +1,15 @@
 from flask import Blueprint
+from use import load_model_and_make_prediction
 
 ai_blueprint = Blueprint("first", __name__, url_prefix="/model")
 
+# X array should be given in the request under /predict.
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+
 @ai_blueprint.route("/predict")
 def prediction():
-    return "Prediction will be here"
+    predicted = load_model_and_make_prediction(x, '900000.pt')
+    return predicted
 
 
 @ai_blueprint.route("/statistics")
