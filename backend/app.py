f832d1f03d3ff8ee3aa52a94559ce77d4ab81169
Ryszawy <Krafi604@gmail.com>
2023-04-27 17:10:35 +0200

    fixed authorization

--- backend/app.py
+++ backend/app.py
@@ -57,6 +57,7 @@ def register_request_filter(app):
             response = make_response()
             response.headers.add('Access-Control-Allow-Credentials', 'true')
             response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+            response.headers.add('Access-Control-Allow-Headers', 'Authorization')
             response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
             response.headers.add('Access-Control-Allow-Methods',
                                 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
@@ -64,6 +65,11 @@ def register_request_filter(app):
                 response.headers.add('Access-Control-Allow-Origin', origin)
         else:
             response.headers.add('Access-Control-Allow-Credentials', 'true')
+            response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+            response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+            response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
+            response.headers.add('Access-Control-Allow-Methods',
+                                'GET, POST, OPTIONS, PUT, PATCH, DELETE')
             if origin:
                 response.headers.add('Access-Control-Allow-Origin', origin)
 
