767ddf9eba161f471015d6f4e14150f0f4c83e5a
Jakub Czyzewski <jakub.czyzewski@tomtom.com>
2023-05-15 23:49:39 +0200

    Add data filter; Create endpoint for all features' description.

--- backend/ai/views.py
+++ backend/ai/views.py
@@ -2,8 +2,10 @@ from flask import Blueprint, request, jsonify
 
 from .data import stats
 from ..ai.use import test
+from ..stats.StatCalculator import StatCalculator
 
 ai_blueprint = Blueprint("ai", __name__, url_prefix="/ai")
+stat_calc = StatCalculator()
 
 
 @ai_blueprint.route("/predict", methods=['POST'])
@@ -19,16 +21,24 @@ def prediction():
 def get_average_likes():
     return stats.load_data('videoLikeCount', "Average Likes")
 
+
 @ai_blueprint.route("/statistics/dislikes")
 def get_average_dislikes():
     return stats.load_data('videoDislikeCount', "Average Dislikes")
 
+
 @ai_blueprint.route("/statistics/comments")
 def get_average_comments():
     return stats.load_data('VideoCommentCount', "Average Comments")
+
 
 @ai_blueprint.route("/statistics/views")
 def get_average_views():
     return stats.load_data('videoViewCount', "Average Views")
 
 
+@ai_blueprint.route("statistics/describe")
+def get_description():
+    stat_calc.reload()
+    stat_calc.filter_data(**request.args)
+    return stat_calc.describe_all_features()
--- backend/stats/StatCalculator.py
+++ backend/stats/StatCalculator.py
@@ -3,12 +3,40 @@ import pandas as pd
 
 class StatCalculator:
 
-    def __init__(self, data='../ai/data/data.csv') -> None:
+    def __init__(self, data='../ai/data/data.csv', **kwargs) -> None:
         super().__init__()
-        self.data = pd.read_csv(data)
+        self.data = None
+        self.data_path = data
+        self.reload()
+        self.filter_data(**kwargs)
 
     def describe_all_features(self):
         return self.data.drop('index', axis=1).describe().to_dict()
 
     def describe_feature(self, column):
-        return self.data.drop('index', axis=1)[column].describe()
+        return self.data.drop('index', axis=1)[column].describe().to_dict()
+
+    def filter_data(self, **kwargs):
+        if kwargs:
+            conditions = ' & '.join(
+                [self.__build_condition(k, v) for k, v in kwargs.items()])
+            self.data = eval(f'self.data[{conditions}]')
+
+    def __build_condition(self, k: str, v):
+        return f'(self.data[\'{self.__remove_prefix(self.__remove_prefix(k, "min"), "max")}\'] ' \
+               f'{self.__get_comparator_sign(k)} {v})'
+
+    @staticmethod
+    def __get_comparator_sign(column):
+        return ">=" if column.startswith("min") else ("<=" if column.startswith("max") else "==")
+
+    @staticmethod
+    def __remove_prefix(text, prefix):
+        return text[text.startswith(prefix) and len(prefix):]
+
+    def reload(self):
+        self.data = pd.read_csv(self.data_path)
+
+
+calc = StatCalculator(CComments=21, minCVideos=8, maxCVideos=12)
+print()
