3f3f6bcf69d3272e9780efc6e405d814bd33a359
KamilPresler <kamilpfcb@gmail.com>
2023-05-18 20:16:33 +0200

    fixed tests for full dataset

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -13,18 +13,18 @@ class TestStats(TestCase):
         self.client = self.app.test_client()
 
     def test_returns_stats_for_music_category(self):
-        params = {'VCategory': 10}
+        params = {'videoCategoryId': 10}
         req = '/ai/statistics/describe?' + urlencode(params)
         res = self.client.get(req).json
 
-        self.assertEqual(res['VViews']['mean'], 101076.14659525632)
-        self.assertEqual(res['VViews']['std'], 1874283.3210626151)
-        self.assertEqual(res['VLikes']['max'], 1240473.0)
+        self.assertEqual(res['videoViewCount']['mean'], 101311.82430352402)
+        self.assertEqual(res['videoViewCount']['std'], 1875953.9900374322)
+        self.assertEqual(res['videoLikeCount']['max'], 1240473.0)
 
     def test_should_filter_music_comments_in_range(self):
-        params = {'VCategory': 10, 'minVComments': 20, 'maxVComments': 30}
+        params = {'videoCategoryId': 10, 'minVideoCommentCount': 20, 'maxVideoCommentCount': 30}
         req = '/ai/statistics/describe?' + urlencode(params)
         res = self.client.get(req).json
 
-        self.assertEqual(res['VComments']['min'], 20)
-        self.assertEqual(res['VComments']['max'], 30)
+        self.assertEqual(res['VideoCommentCount']['min'], 20)
+        self.assertEqual(res['VideoCommentCount']['max'], 30)
