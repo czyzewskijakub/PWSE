fb6c67ed7db2bc4727675c5c8077f598b797e25b
Jakub Czyzewski <jakub.czyzewski@tomtom.com>
2023-05-24 23:28:40 +0200

    Add authorization to tests.

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -1,16 +1,35 @@
+import json
 import os
+import random
+import string
 import sys
 from unittest import TestCase
-from urllib.parse import urlencode
 
 sys.path.append(os.getcwd() + '/..')
 from backend.app import create_app
+
+
+def random_chars(char_num):
+    return "".join(random.choice(string.ascii_letters) for _ in range(char_num))
+
+
+random_email = random_chars(15) + "@" + random_chars(5) + "." + random_chars(2)
+request_body = {
+    "name": "example",
+    "email": random_email.lower(),
+    "password": "14km77So9x!",
+    "profile_picture_url": "https://i.pinimg.com/550x/ee/f5/a8/eef5a896701a544d1b3da168956eca44.jpg"
+}
+token = ""
 
 
 class TestPredictions(TestCase):
     def setUp(self):
         self.app = create_app()
         self.client = self.app.test_client()
+        self.client.post("/users/register", json=request_body, content_type="application/json")
+        response = self.client.post("/users/login", json=request_body, content_type="application/json")
+        self.headers = {'Authorization': f'Bearer {json.loads(response.data)["token"]}'}
 
     def test_should_return_ok_on_prediction(self):
         request_body = {
@@ -27,7 +46,7 @@ class TestPredictions(TestCase):
             "video_published": "2023-06-04",
         }
         req = '/ai/predict'
-        self.assertEqual(self.client.post(req, json=request_body).status_code, 200)
+        self.assertEqual(self.client.post(req, json=request_body, headers=self.headers).status_code, 200)
 
     def test_should_return_predictions_as_number(self):
         request_body = {
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,4 +1,7 @@
+import json
 import os
+import random
+import string
 import sys
 from unittest import TestCase
 from urllib.parse import urlencode
@@ -7,15 +10,32 @@ sys.path.append(os.getcwd() + '/..')
 from backend.app import create_app
 
 
+def random_chars(char_num):
+    return "".join(random.choice(string.ascii_letters) for _ in range(char_num))
+
+
+random_email = random_chars(15) + "@" + random_chars(5) + "." + random_chars(2)
+request_body = {
+    "name": "example",
+    "email": random_email.lower(),
+    "password": "14km77So9x!",
+    "profile_picture_url": "https://i.pinimg.com/550x/ee/f5/a8/eef5a896701a544d1b3da168956eca44.jpg"
+}
+token = ""
+
+
 class TestStats(TestCase):
     def setUp(self):
         self.app = create_app()
         self.client = self.app.test_client()
+        self.client.post("/users/register", json=request_body, content_type="application/json")
+        response = self.client.post("/users/login", json=request_body, content_type="application/json")
+        self.headers = {'Authorization': f'Bearer {json.loads(response.data)["token"]}'}
 
     def test_returns_stats_for_music_category(self):
         params = {'videoCategoryId': 10}
         req = '/ai/statistics/describe?' + urlencode(params)
-        res = self.client.get(req).json
+        res = self.client.get(req, headers=self.headers).json
 
         self.assertEqual(res['videoViewCount']['mean'], 101311.82430352402)
         self.assertEqual(res['videoViewCount']['std'], 1875953.9900374322)
@@ -24,11 +44,11 @@ class TestStats(TestCase):
     def test_should_filter_music_comments_in_range(self):
         params = {'videoCategoryId': 10, 'minVideoCommentCount': 20, 'maxVideoCommentCount': 30}
         req = '/ai/statistics/describe?' + urlencode(params)
-        res = self.client.get(req).json
+        res = self.client.get(req, headers=self.headers).json
 
         self.assertEqual(res['VideoCommentCount']['min'], 20)
         self.assertEqual(res['VideoCommentCount']['max'], 30)
 
     def test_should_return_ok_on_stats(self):
         req = '/ai/statistics/describe'
-        self.assertEqual(self.client.get(req).status_code, 200)
+        self.assertEqual(self.client.get(req, headers=self.headers).status_code, 200)
